fec9d34d434c44a1be5f5a9851e5aaa809155040
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-19 20:21:06 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages.each do |style|
+    languages[style].each do |language, language_data|
+      if !(new_hash.has_key?(language))
+       language_data[:style]=[style]
+       new_hash[language] = language_data
+      else new_hash[language][:style]<< style
+      end
+    end
+  end
+return new_hash
 end
